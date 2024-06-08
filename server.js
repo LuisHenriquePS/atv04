5fa680022cfcef11ab9f535b62310aa07b0850d8
aluno31-pfsii <luis.tec32@gmail.com>
2024-06-08 20:35:39 -0300

    2

--- server.js
+++ server.js
@@ -7,3 +7,4 @@ const server = http.createServer(app);
 server.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
+// docker run -p 3000:3001 --name aula-cc-new -d leroilui31/atv04ead:0.0.1 - pra rodar
